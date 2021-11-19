5cd870479eada01cb6fda1219b58304d24ff098d
Asitha De Alwis <w.dealwis@ucdconnect.ie>
2021-11-19 20:05:30 +0530

    Minor corrections

--- src/app/pages/master-data/ledger-account.service.ts
+++ src/app/pages/master-data/ledger-account.service.ts
@@ -8,12 +8,28 @@ import {ApiResponse} from '../../model/api-model';
 })
 export class LedgerAccountService {
 
-  private readonly baseURL: string = '';
+  private readonly baseURL: string = '/master-data/ledger-accounts';
 
   constructor(private provider: ProviderService) {
   }
 
+  public fetchAll(): Observable<ApiResponse> {
+    return this.provider.get(this.baseURL);
+  }
+
   public fetchActive(): Observable<ApiResponse> {
     return this.provider.get(`${this.baseURL}/active`);
+  }
+
+  public fetch(id: string): Observable<ApiResponse>  {
+    return this.provider.get(`${this.baseURL}/${id}`);
+  }
+
+  public create(data: any): Observable<ApiResponse>  {
+    return this.provider.post(this.baseURL, data);
+  }
+
+  public update(id: string, data: any): Observable<ApiResponse>  {
+    return this.provider.put(`${this.baseURL}/${id}`, data);
   }
 }
